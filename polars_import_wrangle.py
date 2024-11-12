d3aedd47e39b82efcafdd9798c555d640cb6ded3
stevecrawshaw <murrayjarvis@gmail.com>
2024-11-12 09:03:23 +0000

    update

--- polars_import_wrangle.py
+++ polars_import_wrangle.py
@@ -7,18 +7,20 @@ path1 = "data/nr_2023_1.csv" #9GB
 path2 = "data/nr_2023_3.csv" #24GB
 #%%
 
-raw = pl.scan_csv(path1)
+raw = pl.scan_csv(path2)
 
 
 #%%
 %%time
 out_df = (raw
  .filter(pl.col("latitude").is_between(51, 52))
-#  .filter(pl.col("longitude").is_between(-3, -2))
+ .filter(pl.col("longitude").is_between(-3, -1))
 ).collect()
 
 #%%
 out_df.schema
 
 
+#%%
+out_df.glimpse()
 #%%
