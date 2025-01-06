366b4339b36bd08dcc568392dbd2383f7795b8b7
Steve Crawshaw <murrayjarvis@gmail.com>
2025-01-06 10:55:54 +0000

    code club ready

--- ofcom.sql
+++ ofcom.sql
@@ -1,5 +1,6 @@
 -- Use duckdb to wrangle large data files from OFCOM which hold signal strength
 -- data for the UK. The data is in CSV format and is larger than memory (~24GB per file). 
+
 -- The end result will be a GIS data file which represents median signal strength from all
 -- mobile operators at a 1km grid square resolution for the roads driven.
 
@@ -29,7 +30,9 @@ SELECT plan_no, shape.from_hex().ST_GeomFromWKB() AS geom
 FROM weca_postgres.os.grid_1km
 WHERE plan_no LIKE 'ST%'
 LIMIT 2;
+
 -- Now create a table to store the grid data
+-- duckdb supports function chaining like in python libraries
 CREATE TABLE IF NOT EXISTS ofcom.os_gb_grids AS
 SELECT plan_no, shape.from_hex().ST_GeomFromWKB() AS geom
 FROM weca_postgres.os.grid_1km
@@ -52,14 +55,23 @@ WHERE
 AND 
 (longitude BETWEEN -3.021 AND -2.252);
 
+SELECT COUNT() FROM ofcom.nr_2022_2_tbl;
+
 FROM ofcom.nr_2022_2_tbl LIMIT 2;
 .mode line
 
-FROM ofcom.nr_2022_2_tbl LIMIT 2;
+FROM ofcom.nr_2022_2_tbl
+USING SAMPLE 1%
+LIMIT 2;
+
 .mode duckbox
 DESCRIBE ofcom.nr_2022_2_tbl;
 -- get a summary of the data with key stats (only a subset displayed here for demo)
-SUMMARIZE FROM ofcom.nr_2022_2_tbl LIMIT 100000;
+
+EXPLAIN ANALYZE 
+SUMMARIZE 
+FROM ofcom.nr_2022_2_tbl 
+LIMIT 1000000;
 
 -- create a geometry column from the lat/long columns
 -- reprojecting to British National Grid to match with the grid data
@@ -69,45 +81,29 @@ UPDATE ofcom.nr_2022_2_tbl
 SET geom = ST_Transform(ST_Point(latitude, longitude), 'EPSG:4326', 'EPSG:27700');
 
 -- verify the geometry column
-.mode line
 SELECT geom FROM ofcom.nr_2022_2_tbl LIMIT 2;
-.mode duckbox
 
--- clip (intersect) the point data to the grid data and save in a view
--- this assigns a grid square to each point
-CREATE OR REPLACE VIEW labelled_point_vw AS
-SELECT ofcom.os_gb_grids.plan_no
-    , COLUMNS('rssi*|sinr*|pci*')
-    , ST_Intersection(ofcom.nr_2022_2_tbl.geom, ofcom.os_gb_grids.geom) AS intersection
-FROM ofcom.nr_2022_2_tbl , ofcom.os_gb_grids 
-WHERE ST_Intersects(ofcom.nr_2022_2_tbl.geom, ofcom.os_gb_grids.geom);
-
-FROM labelled_point_vw LIMIT 2;
-
--- calculate the grouped signal strength stats for each grid tile
--- and join back to the grid data to capture the grid geometry
+-- spatial join and summarise the signal strength data by grid square in one step
 CREATE OR REPLACE VIEW grid_stats_geom_vw AS
-WITH cte_grid_stats AS
-    (SELECT plan_no, MEDIAN(COLUMNS('rssi*|sinr*')), COLUMNS('pci*')
-    FROM labelled_point_vw
-    GROUP BY plan_no, pci_top1_3uk, pci_top1_ee, pci_top1_o2, pci_top1_vf)
--- now join the cte stats to the grid file, keeping only those that match    
-SELECT ofcom.os_gb_grids.plan_no, geom, COLUMNS('rssi*|sinr*|pci*')
-FROM ofcom.os_gb_grids
-JOIN cte_grid_stats
-ON ofcom.os_gb_grids.plan_no = cte_grid_stats.plan_no;
+SELECT plan_no, MEDIAN(COLUMNS('rssi*|sinr*')),  os_gb_grids.geom
+FROM ofcom.nr_2022_2_tbl , ofcom.os_gb_grids 
+WHERE st_within(ofcom.nr_2022_2_tbl.geom, ofcom.os_gb_grids.geom)
+GROUP BY ALL;
 
 -- Introspect the view, filtering on columns that are not null
+
 FROM grid_stats_geom_vw
 WHERE (rssi_top1_3uk IS NOT NULL) AND (rssi_top1_ee IS NOT NULL)
 LIMIT 2;
 
 -- See if we can export to a modern GIS format like FlatGeoBuf
 SELECT * FROM ST_Drivers()
-WHERE short_name LIKE '%Flat%';
+WHERE short_name LIKE '%Geo%';
 
 -- save the grid stats to a flatgeobuf file for use in QGIS
 COPY (SELECT * FROM grid_stats_geom_vw) 
 TO 'data/grid_stats_geom.fgb' 
 WITH (FORMAT GDAL, DRIVER 'FlatGeobuf');
+
+-- open QGIS to verify the data
 .quit
